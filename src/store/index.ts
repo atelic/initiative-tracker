3c4df09c7be9acc42846e98da272e6bab370699c
Eric Barbour <eric.barbour@willowtreeapps.com>
2020-07-29 20:24:03 -0400

    Set up prettier and set title

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,14 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue';
+import Vuex from 'vuex';
+import { RootState } from '@/store/models';
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-  state: {},
+export default new Vuex.Store<RootState>({
+  state: {
+    characters: Array<string>(),
+  },
   mutations: {},
   actions: {},
-  modules: {}
+  modules: {},
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld msg="Initiative Tracker" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Vue from 'vue';
+import HelloWorld from '@/components/HelloWorld.vue';
 
-export default {
-  name: "Home",
+export default Vue.extend({
+  name: 'Home',
   components: {
-    HelloWorld
-  }
-};
+    HelloWorld,
+  },
+});
 </script>
